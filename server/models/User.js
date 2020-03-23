bb611fd0480aae02845ebcf2b83a4c42613fc14c
maeng2418 <maeng2418@github.com>
2020-03-23 13:46:41 +0900

    5. Encrypt password with Bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); // 비밀번호 암호화 하기
+const saltRounds = 10; // Salt를 이용해서 비밀번호를 암호화 => salt를 10자리로 설정.
 
 const userSchema = mongoose.Schema({
     name: {
@@ -33,6 +35,27 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+// 정보들을 유저모델을 저장하기 전에 실행해줌.
+userSchema.pre('save', function( next ){
+    var user = this; // userSchema를 가리키게 됨.
+
+    if(user.isModified('password')){ // 비밀번호가 바뀔 때만 암호화
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) { // Salt 생성
+            if(err) return next(err);
+            bcrypt.hash(user.password, salt, function (err, hash){
+                if(err) return next(err);
+                // Store hash in you password DB
+                user.password = hash // 암호화된 비밀번호로 교체
+                next(); // 다음꺼 실행(유저모델 저장)
+            });
+        });
+    } else { // 비밀번호가 아닌 다른정보 바꿀 시, 그냥 넘어감(next())
+        next();
+    }
+});
+
 
 const User = mongoose.model('User', userSchema); // schema를 모델로 감싸줌. (모델이름, 스키마)
 
