f0acb9ee94c83d6ec173506cc16d0c07eafb41da
maeng2418 <maeng2418@github.com>
2020-03-23 13:20:56 +0900

    2. Connect MongoDB

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const config = require('../config/key'); // mongoURI 가져오기
+
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false // 오류 막기 위함
+}).then(() => console.log('MongoDB connected...')).catch(err => console.log(err));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
